cf17deddd2a1937a033def82a6ff92a68f25619d
Dhani Baksh <dhanibaksh781@gmail.com>
2026-07-08 23:38:12 +0500

    Add files via upload

---
+++ groq_client.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from groq import Groq
+import os
+
+
+client = Groq(api_key=os.getenv("groq_api_key"))
+def get_roast(message : list):
+    response = client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        messages = message
+    )
+    return response.choices[0].message.content
